0b3a899c73bae250d1f248f2fb2b4905ce28a5f4
Ryo-pow <shennongliangtai@gmail.com>
2025-12-02 11:11:26 +0900

    文法の練習

---
+++ Main.scala
@@ -0,0 +1,25 @@
+object VariableExercise {
+  def main(args: Array[String]): Unit = {
+    
+        val pi = 3.14159
+
+        println(s"円周率:${pi}")
+
+
+
+        val companyName = "FutureTech Inc."
+
+        println(s"会社名:${companyName}")
+
+
+    
+        println("---")
+    
+        // 類題2: var の定義と再代入
+        var temperature = 28        // var temperature = ???
+        println(temperature)        // println(temperature)
+        temperature = 35            // temperature = ??? // 再代入
+        println(s"${temperature}")  // println(temperature)
+    
+    }
+}
