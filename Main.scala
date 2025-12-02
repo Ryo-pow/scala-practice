f9d70e5053fdaceeba6ca267ee89563080edc711
Ryo-pow <shennongliangtai@gmail.com>
2025-12-02 14:45:49 +0900

    if,for,の使い方を学んだ

--- Main.scala
+++ Main.scala
@@ -1,25 +1,23 @@
-object VariableExercise {
-  def main(args: Array[String]): Unit = {
+object AdvancedExercise {
+    def calculateSalary(hourlywage: Int,workinghours: Int) = if (workinghours > 40){
+        hourlywage * 40 + hourlywage * (workinghours -40) * 125 / 100
+    } else{
+        hourlywage * workinghours
+    }
+
+
+    def main(args: Array[String]): Unit = {
     
-        val pi = 3.14159
+        val hourlywage = 1000
+        val workinghours = 45
+        val salary = calculateSalary(hourlywage, workinghours)
+        println(s"時給 ${hourlywage}円で ${workinghours}時間働いた総支給額 ${salary}円")
 
-        println(s"円周率:${pi}")
+        val oddSquares = for {
+            i <- 1 to 10
+            if (i % 2 != 0) 
+        }yield i * i
 
-
-
-        val companyName = "FutureTech Inc."
-
-        println(s"会社名:${companyName}")
-
-
-    
-        println("---")
-    
-        // 類題2: var の定義と再代入
-        var temperature = 28        // var temperature = ???
-        println(temperature)        // println(temperature)
-        temperature = 35            // temperature = ??? // 再代入
-        println(s"${temperature}")  // println(temperature)
-    
+        println(s"奇数の二乗リスト： ${oddSquares}")
     }
 }
